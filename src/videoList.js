80dd8e77789b30aead65c5d0848ea0cb965f16ba
Samuel <sam@vd.st>
2024-10-01 19:20:38 +0200

    a bit of cleanup (#13)

---
+++ src/videoList.js
@@ -0,0 +1,76 @@
+export const videos = [
+  {
+    title: "MUTTENALP",
+    category: "a",
+    youtube_url: "https://www.youtube.com/watch?v=iEScntxKfHc&vq=hd2160",
+    hosted_video_url:
+      "https://pub-72a1e46318194326b09d8d7e598a9615.r2.dev/Muttenalp.webm",
+    description:
+      "Videobeschribig do ahne. Hello I'm at least way to fuul zum was schribe darum: Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    title: "ALICANTE",
+    category: "a",
+    youtube_url: "https://www.youtube.com/watch?v=3hW7AbfMBqI&vq=hd2160",
+    hosted_video_url:
+      "https://pub-72a1e46318194326b09d8d7e598a9615.r2.dev/ALICANTE.webm",
+    description:
+      "Videobeschribig do ahne. Hello I'm at least way to fuul zum was schribe darum: Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    title: "MATEC x DRIFTANIC",
+    category: "a",
+    youtube_url: "https://www.youtube.com/watch?v=gMvDwypLNLM&vq=hd2160",
+    hosted_video_url:
+      "https://pub-72a1e46318194326b09d8d7e598a9615.r2.dev/MATEC%20x%20DRIFTANIC%20v3.0%20Volvo%20945.webm",
+    description:
+      " MATEC x DRIFTANIC v3.0 // Volvo 945 - Is a Video I made for a Friend drifting his car which was made by another friend. Duh. Videobeschribig do ahne. Hello I'm at least way to fuul zum was schribe darum: Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+  },
+  {
+    title: "TENERIFE",
+    category: "a",
+    youtube_url: "https://www.youtube.com/watch?v=GY9u90LHCnQ&vq=hd1080",
+    hosted_video_url:
+      "https://pub-72a1e46318194326b09d8d7e598a9615.r2.dev/Tenerife.mp4",
+    description:
+      " MATEC x DRIFTANIC v3.0 // Volvo 945 - Is a Video I made for a Friend drifting his car which was made by another friend. Duh. Videobeschribig do ahne. Hello I'm at least way to fuul zum was schribe darum: Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+  },
+  {
+    title: "CONDENSATION [7]",
+    category: "b",
+    youtube_url: "https://www.youtube.com/watch?v=Jy_G2DbyrEI&vq=hd2160",
+    hosted_video_url:
+      "https://pub-72a1e46318194326b09d8d7e598a9615.r2.dev/Condensation%20%5B7%5D.webm",
+    description:
+      " MATEC x DRIFTANIC v3.0 // Volvo 945 - Is a Video I made for a Friend drifting his car which was made by another friend. Duh. Videobeschribig do ahne. Hello I'm at least way to fuul zum was schribe darum: Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+  },
+  {
+    title: "ANDERMATT // GEMSSTOCK",
+    category: "b",
+    youtube_url: "https://www.youtube.com/watch?v=J9KUrYSbrtE&vq=hd2160",
+    hosted_video_url:
+      "https://pub-72a1e46318194326b09d8d7e598a9615.r2.dev/Andermatt%20Gemsstock.webm",
+    description:
+      " MATEC x DRIFTANIC v3.0 // Volvo 945 - Is a Video I made for a Friend drifting his car which was made by another friend. Duh. Videobeschribig do ahne. Hello I'm at least way to fuul zum was schribe darum: Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+  },
+  {
+    title: "SOMEWHERE",
+    category: "b",
+    youtube_url: "https://www.youtube.com/watch?v=VPzKhhH16F0&vq=hd2160",
+    hosted_video_url:
+      "https://pub-72a1e46318194326b09d8d7e598a9615.r2.dev/SOMEWHERE.webm",
+    description:
+      " MATEC x DRIFTANIC v3.0 // Volvo 945 - Is a Video I made for a Friend drifting his car which was made by another friend. Duh. Videobeschribig do ahne. Hello I'm at least way to fuul zum was schribe darum: Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+  },
+  {
+    title: "OUR CHRISTMAS",
+    category: "b",
+    youtube_url: "https://www.youtube.com/watch?v=kZADnEeXTFA&vq=hd2160",
+    hosted_video_url:
+      "https://pub-72a1e46318194326b09d8d7e598a9615.r2.dev/Our%20Christmas.webm",
+    description:
+      " MATEC x DRIFTANIC v3.0 // Volvo 945 - Is a Video I made for a Friend drifting his car which was made by another friend. Duh. Videobeschribig do ahne. Hello I'm at least way to fuul zum was schribe darum: Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+  },
+];
+
+export const niceVideos = videos.filter((vid) => vid.category === "b");
